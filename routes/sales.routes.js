93f296736288ca7782f36b599a6b1c2bd82ed641
barbearia-suazo-site <suazo@suazobarber.com>
2025-11-14 14:41:58 +0100

    Update sales.routes.js

--- routes/sales.routes.js
+++ routes/sales.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import { verifyToken, requireAdmin } from "../middlewares/auth.js"; // protege com login JWT
+import { verifyToken, requireAdmin } from "../middleware/auth.js"; // protege com login JWT
 
 const router = express.Router();
 
